41437d70b9db85f4ba47b2e2b2f328a7fa8321ac
Liam Kirsh <liam.kirsh@gmail.com>
2016-10-19 13:09:58 -0700

    app: cleanup and structure

---
+++ app/views.py
@@ -0,0 +1,37 @@
+from app import app
+import facebook
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route('/group')
+def group():
+    # User access token expires after a couple hours; need to add login
+    graph = facebook.GraphAPI('EAACEdEose0cBALHtWmGPrk2hzcAj5Onj2RZBZBy6zGIkNVgjvvt043VmJwCBZBLhnrGZB9YVJy6bsZCu4WgqMAxowSqLKyGurBMjCN0UjZAhI3f3GJQ8vXKDgBpziZALniCQSsx8uZCvHWFW0wH1BYdUTuIiam0nQX9D7hDz932qjAZDZD')
+    ffs_group_id = '401906879833440'
+    ip_group_id = '341686389286734'
+
+    posts = graph.get_object(ffs_group_id + '/feed')
+
+    postNum = 1
+    curPost = ''
+    
+    for post in posts['data']:
+        curPost += '<br>POST #' + str(postNum) + '<br>'
+		
+        curPost += post['message']
+
+        curPost += '<br>'
+
+        try:
+            curPost += '<img src="' + post['picture'] + '">'
+        except KeyError:
+            curPost += 'No picture included'
+
+            curPost += '<br>'
+            postNum += 1
+
+        return curPost
+
