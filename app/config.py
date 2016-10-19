1b99bf41242608dc5e3f6b229efa67decbdd2fad
Liam Kirsh <liam.kirsh@gmail.com>
2016-10-19 15:31:43 -0700

    structure: added templates; added basic FB login js (not working)

---
+++ app/config.py
@@ -0,0 +1,8 @@
+""" Configuration variables. """
+
+import os
+
+
+FB_APP_ID = os.environ.get("FB_APP_ID")
+FB_APP_NAME = os.environ.get("FB_APP_NAME")
+FB_APP_SECRET = os.environ.get("FB_APP_SECRET")
--- app/views.py
+++ app/views.py
@@ -1,15 +1,20 @@
+import os
+
 from app import app
-import facebook
+from flask import render_template
+
+from facebook import get_user_from_cookie, GraphAPI
+from .config import FB_APP_ID
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', app_id = os.environ.get("FB_APP_ID"))
 
 @app.route('/group')
 def group():
     # User access token expires after a couple hours; need to add login
-    graph = facebook.GraphAPI('EAACEdEose0cBALHtWmGPrk2hzcAj5Onj2RZBZBy6zGIkNVgjvvt043VmJwCBZBLhnrGZB9YVJy6bsZCu4WgqMAxowSqLKyGurBMjCN0UjZAhI3f3GJQ8vXKDgBpziZALniCQSsx8uZCvHWFW0wH1BYdUTuIiam0nQX9D7hDz932qjAZDZD')
+    graph = GraphAPI('EAACEdEose0cBALHtWmGPrk2hzcAj5Onj2RZBZBy6zGIkNVgjvvt043VmJwCBZBLhnrGZB9YVJy6bsZCu4WgqMAxowSqLKyGurBMjCN0UjZAhI3f3GJQ8vXKDgBpziZALniCQSsx8uZCvHWFW0wH1BYdUTuIiam0nQX9D7hDz932qjAZDZD')
     ffs_group_id = '401906879833440'
     ip_group_id = '341686389286734'
 
