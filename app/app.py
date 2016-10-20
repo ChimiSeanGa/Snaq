4b857f5aeea59f714aa709b869f5098ca0d5f42f
ChimiSeanGa <seang696@gmail.com>
2016-10-20 11:59:26 -0700

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,46 @@
+import fbconsole
+import flask
+from flask import Flask, abort, request
+app = Flask(__name__)
+
+FFS_GROUP_ID = '401906879833440'
+
+@app.route('/authorize')
+def authorize_facebook():
+	fbconsole.AUTH_SCOPE = ['public_profile']
+	fbconsole.authenticate()
+
+	return "Good to go! <a href='/getposts'>Click here!</a>"
+
+@app.route('/')
+def home():
+	# User access token expires after a couple hours; need to add login
+
+	return '<a href="/authorize">Login</a>'
+
+@app.route('/getposts')
+def getposts():
+	posts = fbconsole.get('/' + FFS_GROUP_ID + '/feed')
+
+	postNum = 1
+	curPost = ''
+
+	for post in posts['data']:
+		curPost += '<br>POST #' + str(postNum) + '<br>'
+		
+		curPost += post['message']
+
+		curPost += '<br>'
+
+		try:
+			curPost += '<img src="' + post['picture'] + '">'
+		except KeyError:
+			curPost += 'No picture included'
+
+		curPost += '<br>'
+		postNum += 1
+
+	return curPost
+
+if __name__ == "__main__":
+   app.run()
